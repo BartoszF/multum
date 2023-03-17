e2cc80af8a3d9f38a3368bb0cca112e31125cce8
BartoszF <felis.bartosz@gmail.com>
2023-03-17 19:35:36 +0100

    Add publishing to common

--- examples/discoveryClient/build.gradle.kts
+++ examples/discoveryClient/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     }
 
     implementation(project(":modules:multum-client"))
-//    implementation("com.github.BartoszF:multum:0.0.3")
+//    implementation("com.github.BartoszF:multum:0.1.0")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- modules/multum-common/build.gradle.kts
+++ modules/multum-common/build.gradle.kts
@@ -3,6 +3,7 @@ val ktor_version: String by project
 plugins {
     kotlin("jvm") version "1.8.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("maven-publish")
 }
 
 repositories {
@@ -11,4 +12,16 @@ repositories {
 
 dependencies {
     api("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "pl.felis"
+            artifactId = "multum-common"
+            version = version
+
+            from(components["java"])
+        }
+    }
 }
